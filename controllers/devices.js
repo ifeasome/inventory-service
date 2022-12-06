7693012606652904660712c4c5115a289163c97e
Somelina Obiechina <ifeasome@gmail.com>
2022-12-05 21:21:08 -0600

    websockets addedd

--- controllers/devices.js
+++ controllers/devices.js
@@ -1,3 +1,4 @@
+const ws = require('../config/socket');
 const { Device } = require('../models');
 
 module.exports = {
@@ -10,5 +11,39 @@ module.exports = {
         payload: rows
       })
     );
+  },
+  addDevice: async (payload) => {
+    const { rows } = await Device.create(payload);
+  
+    ws.broadcast(
+      JSON.stringify({
+        type: 'add-device',
+        payload: rows[0]
+      })
+    );
+  },
+  updateDevice: async (payload) => {
+    const { rows, rowCount } = await Device.update(payload);
+  
+    if (rowCount > 0) {
+      ws.broadcast(
+        JSON.stringify({
+          type: 'update-device',
+          payload: rows[0]
+        })
+      );
+    }
+  },
+  removeDevice: async (payload) => {
+    const { rowCount } = await Device.delete(payload);
+  
+    if (rowCount > 0) {
+      ws.broadcast(
+        JSON.stringify({
+          type: 'remove-device',
+          payload: { id: payload.id }
+        })
+      );
+    }
   }
 };
